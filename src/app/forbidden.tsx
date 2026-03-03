f814f6301bd8d9c0ffb74b719fa9879ab0ba0e6b
Daniel <daniel@vmi3089388.contaboserver.net>
2026-03-03 17:11:22 +0100

    fix: add use client to forbidden and not-found pages

--- src/app/forbidden.tsx
+++ src/app/forbidden.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { ShieldX, ArrowLeft } from "lucide-react";
 import Link from "next/link";
 
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { FileQuestion, Home, ArrowLeft } from "lucide-react";
 import Link from "next/link";
 
